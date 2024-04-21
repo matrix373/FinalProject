622dc900803267b3cd10ec71275f1ea6a1ac857c
Efe Caylan <103333941+matrix373@users.noreply.github.com>
2024-04-21 23:07:21 +0000

    Netlify deployment adjustments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'; 
+import React, { useState } from 'react'; 
 import './App.css';
 import { GoogleMap, LoadScript} from '@react-google-maps/api';
 
