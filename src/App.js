ec8f50ed8dda1029cf50b353ada96c643e774f87
Efe Caylan <103333941+matrix373@users.noreply.github.com>
2024-04-21 22:59:37 +0000

    Styling added to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import './App.css';
-import { GoogleMap, LoadScript, useJsApiLoader } from '@react-google-maps/api';
-
+import { GoogleMap, LoadScript} from '@react-google-maps/api';
 
 
 const containerStyle = {
-  width: '40vw', 
-  height: '40vh',
-  margin: '100px'
+  width: '100%', 
+  height: '100%',
 };
 
 const center = {
@@ -17,11 +15,12 @@ const center = {
 
 
 function App() {
-  const [currencyConverter, setCurrencyConverter] = useState('');
+  const [currencyConverter, setCurrencyConverter] = useState('1 USD = 1.00000 USD');
+  const [countryGetter, setCountryGetter] = useState('United States');
 
   const getCurrencyCode = async (countryISOCode) => {  
-
-    const data = await fetch(`https://api-bdc.net/data/country-info?code=${countryISOCode}&localityLanguage=en&key=bdc_7fe4975bd35841baa0e5561160caa837`);
+    const bdc_key = process.env.REACT_APP_BDC_API_KEY;
+    const data = await fetch(`https://api-bdc.net/data/country-info?code=${countryISOCode}&localityLanguage=en&key=${bdc_key}`);
     if(data.ok) {
       const dataSet = await data.json();
       return dataSet.currency.code;
@@ -29,12 +28,16 @@ function App() {
   };
 
   const convertCurrency = async (currencyCode) => {
-    const data = await fetch(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=6583d5d1ef914621a7720f63f5b3b4e9&symbols=${currencyCode},USD`);
+    const freaks_key = process.env.REACT_APP_CURRENCY_FREAKS_API_KEY;
+    const data = await fetch(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${freaks_key}&symbols=${currencyCode},USD`);
     if(data.ok) {
       const dataSet = await data.json();
       const rate = dataSet.rates[currencyCode];
       const displayRate = parseFloat(rate).toFixed(5);
       return `1 USD = ${displayRate} ${currencyCode}`;
+    }
+    else {
+      return `1 USD = 1.00000 USD`;
     }
   }
 
@@ -52,28 +55,45 @@ function App() {
           const currencyCode = await getCurrencyCode(countryISOCode);
           const exchangeRate = await convertCurrency(currencyCode);
           setCurrencyConverter(exchangeRate);
-
+          setCountryGetter(countryName);
           break;
         }
       }
     });
   };
 
-  
-
   return (
-    <div>
-      <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onClick={handleMapClick}
-      >
-      </GoogleMap>
-      <div>
-        <p>{currencyConverter}</p>
+    <LoadScript 
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      language='en'
+      version='weekly'
+    >
+      <div className="container-fluid text-center p-0">
+        <div className="row">
+          <div className="col">
+            <h1 className="display-4 text-white currency-header w-100 p-3">CURRENCY EXCHANGE RATES</h1>
+            <img src="/Frame1.png" alt="Choose a country" className="img-fluid" />
+          </div>
+        </div>
+        <div className="row align-items-end static-background p-5">
+          <div className="col-md-5 mb-3" >
+            <div className="info-card text-white response-backgournd p-3 shadow rounded d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '350px' }}>
+              <h2 className="my-2 fs-1 fw-bold">{countryGetter}</h2>
+              <p className="lead my-2 fs-2">{currencyConverter}</p>
+            </div>
+          </div>
+          <div className="col-md-7" style={{ height: `50vh` }}>        
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+              onClick={handleMapClick}
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </LoadScript>
+    
     
   );
 }
